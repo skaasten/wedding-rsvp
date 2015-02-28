6bbe9d03d56122db9c7bd9358d0e30f8fc61ffb5
Shaun Kaasten <shaunk@gmail.com>
2015-02-27 23:59:57 -0500

    send email on rsvp

--- index.js
+++ index.js
@@ -33,8 +33,37 @@ var updateSpreadsheet = function(rsvpInfo) {
 var express = require('express');
 var app = express();
 
+var nodemailer = require("nodemailer");
+var smtpTransport = require('nodemailer-smtp-transport');
+var options = {
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    debug: true,
+    secure: true,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+};
+console.log(options);
+var transporter = nodemailer.createTransport(smtpTransport(options));
+var sendMail = function(info) {
+    transporter.sendMail({
+        from: process.env.EMAIL_USER,
+        to: process.env.EMAIL_TO,
+        subject: "New Wedding RSVP", // Subject line
+        text: info
+    }, function(error, response){
+        if(error){
+            console.log(error);
+        } else{
+            console.log("Message sent: " + response);
+        }
+    });
+};
+
 app.post('/rsvp', function(req, res) {
-    updateSpreadsheet('hello world');
+    sendMail(req.body);
     res.send('ok');
 });
 
