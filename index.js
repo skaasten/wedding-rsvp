8c0902a074425f11254b9aedecedd9485d6c7043
Shaun Kaasten <shaunk@gmail.com>
2015-03-14 23:10:30 -0400

    Put rsvps in database

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ var options = {
     }
 };
 
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME
+});
+
 var logMessage = function() {
     if (process.env.NODE_ENV == 'production') {
         return;
@@ -43,6 +51,16 @@ var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 
 app.post('/rsvp/rsvp', jsonParser, function(req, res) {
+    rsvp = {
+        name: req.body.name,
+        meal_choice: req.body.meal_choice,
+        guest: req.body.guest,
+        guest_meal: req.body.guest_meal
+    };
+    logMessage(connection);
+    connection.query('INSERT INTO rsvps SET ?', rsvp, function(err, result){
+        logMessage('inserted rsvp into database:', err, result);
+    });
     sendMail(req.body);
     res.send('ok');
 });
@@ -55,4 +73,11 @@ var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
   logMessage('Example app listening at http://%s:%s', host, port);
+  connection.connect(function(err) {
+  if (err) {
+    logMessage('error connecting: ' + err.stack);
+    return;
+  }
+  logMessage('connected as id ' + connection.threadId);
+});
 });
