8b6f89e2c773baff7b209236a0e6f546bfde2ed2
Shaun Kaasten <shaunk@gmail.com>
2015-05-09 16:45:21 -0400

    Track can_attend

--- rsvps.sql
+++ rsvps.sql
@@ -30,11 +30,12 @@ CREATE TABLE `rsvps` (
   `guest_meal` varchar(100) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `email` varchar(100) DEFAULT NULL,
+  `can_attend` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`),
   UNIQUE KEY `guest` (`guest`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -46,4 +47,4 @@ CREATE TABLE `rsvps` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-03-29 20:06:10
+-- Dump completed on 2015-05-09 16:44:39
